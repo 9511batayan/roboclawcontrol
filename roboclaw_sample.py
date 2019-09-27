8232fa36715d4a32878b657e6d438314e7e9d3fa
Koji Kawabata <18kmc08@ms.dendai.ac.jp>
2019-09-27 19:36:06 +0900

    Update roboclaw_sample.py

--- roboclaw_sample.py
+++ roboclaw_sample.py
@@ -9,6 +9,9 @@ import signal
 import sys
 import roboclaw_driver.roboclaw_driver as rc
 
+'''
+# @brief : Function called by 
+'''
 def handler(signal,frame):
 	rc.ForwardMixed(address,0)
 	rc.TurnRightMixed(address,0)
